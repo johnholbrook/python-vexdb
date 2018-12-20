a9f09035791d0b0c16a5cb8fc14a9bf1dbad4c3c
John Holbrook <john@johnholbrook.us>
2018-12-20 16:34:19 -0500

    Add getEventBySKU, getEventName

---
+++ test.py
@@ -0,0 +1,3 @@
+import vexdb as v
+
+print v.getEventName("RE-VRC-18-7200")
--- vexdb.py
+++ vexdb.py
@@ -1,1 +1,22 @@
 import urllib, json
+
+def urlToJSON(url):
+	if type(url) != type("string"):
+		raise TypeError("Parameter 'url' must be a string.")
+	return json.loads(urllib.urlopen(url).read())
+
+def getEventBySKU(sku):
+	if type(sku) != type("string"):
+		raise TypeError("Parameter 'sku' must be a string.")
+	result = urlToJSON("https://api.vexdb.io/v1/get_events?sku=%s" % sku)
+	if result["status"] == 1:
+		return result["result"]
+	# else:
+		#TODO: Raise the appropriate error
+
+def getEventName(sku):
+	if type(sku) != type("string"):
+		raise TypeError("Parameter 'sku' must be a string.")
+	try:
+		return getEventBySKU(sku)[0]["name"]
+	except 
