5b6c05bdfc0053ee786c61f26fd5743acfee8556
John Holbrook <john@johnholbrook.us>
2019-01-04 14:02:40 -0500

    add some additional functions

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 import vexdb as v
 
-print v.getEventName("RE-VRC-18-7200")
+# print v.getEventName("RE-VRC-18-7200")
+
+print v.getEvent(sku="RE-VRC-18-7200")
--- vexdb.py
+++ vexdb.py
@@ -3,20 +3,65 @@ import urllib, json
 def urlToJSON(url):
 	if type(url) != type("string"):
 		raise TypeError("Parameter 'url' must be a string.")
-	return json.loads(urllib.urlopen(url).read())
+	result = json.loads(urllib.urlopen(url).read())
+	if (result["status"] == 1):
+		return result["result"]
+	else:
+		raise RuntimeError("VexDB Server Error: %s" % result["error_text"])
+
+def getEvent(sku=None,
+			 program=None,
+			 date=None,
+			 season=None,
+			 city=None,
+			 region=None,
+			 country=None,
+			 team=None,
+			 status=None):
+	#check for correct  
+
+	#build list of parameters to specify
+	params = "?"
+	if (sku != None):
+		params += "sku=%s&" % sku
+	if (program != None):
+		params += "program=%s&" % program
+	if (date != None):
+		params += "date=%s&" % date
+	if (season != None):
+		params += "season=%s&" % season
+	if (city != None):
+		params += "city=%s&" % city
+	if (region != None):
+		params += "region=%s&" % region
+	if (country != None):
+		params += "country=%s&" % country
+	if (team != None):
+		params += "team=%s&" % team
+	if (status != None):
+		params += "status=%s&" % status
+
+	# print "https://api.vexdb.io/v1/get_events%s" % params
+	try:
+		return urlToJSON("https://api.vexdb.io/v1/get_events%s" % params)
+	except Exception as e:
+		print e
+		raise
+	# event = urlToJSON("https://api.vexdb.io/v1/get_events%s" % params)
+
 
 def getEventBySKU(sku):
 	if type(sku) != type("string"):
 		raise TypeError("Parameter 'sku' must be a string.")
-	result = urlToJSON("https://api.vexdb.io/v1/get_events?sku=%s" % sku)
-	if result["status"] == 1:
-		return result["result"]
-	# else:
-		#TODO: Raise the appropriate error
-
+	try:
+		return urlToJSON("https://api.vexdb.io/v1/get_events?sku=%s" % sku)[0]
+	except Exception as e:
+		print e
+	
 def getEventName(sku):
 	if type(sku) != type("string"):
 		raise TypeError("Parameter 'sku' must be a string.")
 	try:
-		return getEventBySKU(sku)[0]["name"]
-	except 
+		return getEventBySKU(sku)["name"]
+	except Exception as e:
+		print e
