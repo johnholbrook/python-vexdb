1d0d89128edfc0d3588fc727c7d5f41386ba5760
John Holbrook <john@johnholbrook.us>
2019-01-23 15:31:20 -0500

    Add example script high_score.py

---
+++ example_scripts/high_score.py
@@ -0,0 +1,31 @@
+#Finds the highest match score in the current season worldwide,
+#as well as the highest match score in the current season in
+#West Virginia
+
+from __future__ import print_function
+import vexdb as v
+
+num_rankings = v.getNumRankings(season="current")
+print("Getting all %s rankings in the current season..." % num_rankings)
+all_rankings = v.getRankings(season="current", get_all=True)
+
+highest_score = 0
+
+for ranking in all_rankings:
+	if ranking["max_score"] > highest_score:
+		highest_score = ranking["max_score"]
+
+print("Highest Score Worldwide: %s" % highest_score)
+
+all_wv_events = v.getEvents(region="West Virginia", season="current")
+all_wv_rankings = []
+for event in all_wv_events:
+	all_wv_rankings += v.getRankings(sku=event["sku"])
+
+highest_wv_score = 0
+
+for ranking in all_wv_rankings:
+	if ranking["max_score"] > highest_wv_score:
+		highest_wv_score = ranking["max_score"]
+
+print("Highest Score in WV: %s" % highest_wv_score)
